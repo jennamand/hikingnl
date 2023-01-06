382df7620620ab5bef19bc36af6cb60fc1f93448
jennamand <120421051+jennamand@users.noreply.github.com>
2023-01-06 15:44:32 -0330

    Edits

--- src/index.js
+++ src/index.js
@@ -5,4 +5,25 @@ document.getElementById('hard').onclick = function () {
     console.log("Hard")
 }
 
+let array = [
+    ['Easy','Hard'],
+    ['Silver Mine Head Path', 'White Horse Path']
+];
+
+ for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+        console.log(array[i][j]);
+    }
+}
+
+
+function displayRadioValue() {
+    var array = document.getElementsByName('difficulty');
+      
+    for(i = 0; i < array.length; i++) {
+        if(array[i].checked)
+        document.getElementById("trail").innerHTML
+                = "Trail: "+array[i].value;
+
+
 
