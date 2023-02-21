802101cd788d00083ddddce4d3a25bd3534b3089
jennamand <120421051+jennamand@users.noreply.github.com>
2023-02-21 14:22:39 -0330

    Moving the 2D array outside of the function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 
+ // 2D Array
+
+ let hikes = [
+    ['Easy','Silver Mine Head Path', 3.8],
+    ['Hard','White Horse Path', 18.2],
+    ['Easy','Father Troy', 8.9],
+    ['Hard','Spout Path', 16.2]
+];
+    console.log(hikes)
+
 //Allows console.log to display what radio button was selected.
 let chosen;
 
@@ -11,45 +21,58 @@ document.getElementById('hard').onclick = function () {
     chosen = "Hard";
 }
 
+// Select by Difficulty
+
 function displayArray() {
 
-//Two dimensional array
-
-    let hikes = [
-        ['Easy','Silver Mine Head Path', 3.8],
-        ['Hard','White Horse Path', 18.2],
-        ['Easy','Father Troy', 8.9],
-        ['Hard','Spout Path', 16.2]
-    ];
-        console.log(hikes)
-      
     let results = [];
     console.log(results);
-
+      
     for(i = 0; i < hikes.length; i++) {
     
         if(hikes[i][0] == chosen) {  //Checks row for difficulty
             console.log("Row: "+chosen)
 
-                    results.push(hikes[i][1]); //Shows corrisponding hike with chosen difficulty
+            results.push(hikes[i][1]); //Shows corrisponding hike with chosen difficulty
 
-                        document.getElementById("output").innerHTML //Shows users their result
-                        = "Trail: "+results.join(" <br> ");
-
-                
-
-              }
-            }
+            document.getElementById("output").innerHTML = "Trail: "+results.join(" <br> "); //Shows users their result
+           
         }
+    }
+}
 
-// if(hikes[i][0] == chosen) {
-                            
-    //results[i] = hikes[i][1];
+//Slider
 
-        //document.getElementById("output").innerHTML = "Trail: "+results[i];
+let slider = document.getElementById("sliderRange");
+let length = document.getElementById("demo");
+let trailName = document.getElementById("output");
 
-    //}
+length.innerHTML = slider.value;
 
-        //We need to check the row for the difficulty value, compare it with the chosen variable and collect a list of rows that match that criteria
-        //Populate "results" arrays with corresponding hikes
-        //show the users on screen what hike they selected
+slider.oninput = function displayArray() {
+
+    let results = [];
+
+    length.innerHTML = this.value; // Displays slider value
+
+    let selectedTrails = hikes.filter(function(hikes) {
+
+    return hikes[2] == slider.value;
+
+  });
+
+    if(selectedTrails.length > 0) {
+
+        let trailNames = selectedTrails.map(function(hikes) {
+        return hikes[1];
+
+        });
+
+        document.getElementById("output").innerHTML = "Trail name: " + trailNames.join(", ");
+    } 
+    
+    else {
+        document.getElementById("output").innerHTML = "No trail with the selected length found.";
+    }   
+    
+} 
